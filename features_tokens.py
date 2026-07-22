1b701a03b6245c44b2bfbdbc41031c84a651eb11
ethanndaniel <ethan_daniel_magbatoc@dlsu.edu.com.ph>
2026-07-22 18:34:42 +0800

    created feature matrix using sklearn

--- features_tokens.py
+++ features_tokens.py
@@ -3,12 +3,10 @@ import string
 
 def get_features(word):
     return {
-    "==counts=="
         "length": len(word),
         "alphabet_count": sum(c.isalpha() for c in word),
         "digit_count": sum(c.isdigit() for c in word),
 
-    "==booleans=="
         "has_digit": any(c.isdigit() for c in word),
         "has_alphabet": any(c.isalpha() for c in word),
         "has_hyphen": "-" in word,
@@ -16,7 +14,7 @@ def get_features(word):
         "is_alphabet": word.isalpha(),
         "is_upper": word.isupper(),
         "is_lower": word.islower(),
-        "is_capital": word[0].isupper(),
+        "is_capital": len(word) > 0 and word[0].isupper(),
 
         "is_punctuation": all(c in string.punctuation for c in word),
 
--- tokens.py
+++ tokens.py
@@ -7,18 +7,32 @@ OTH = "OTH"
 
 import pandas as pd
 from features_tokens import get_features
+from sklearn.feature_extraction import DictVectorizer
+"== NOTE copy paste this in vscode terminal to install sklearn: 'pip install scikit-learn' =="
+
 
 "read Sentence IDs 2180 to 2200"
 "FORMAT: word_id, sentence_id, sentence, word, label"
 df = pd.read_excel("labeled_tokens.xlsx")
 
 
-"feature matrix; read through each row and get its features and label"
+"read through each row and get its features and label"
 x = []
 y = []
-
 for _, row in df.iterrows():
     x.append(get_features(row["word"]))
     y.append(row["label"])
 
 
+"convert to dicts using sklearn's DictVectorizer"
+vectorizer = DictVectorizer()
+X = vectorizer.fit_transform(x)
+
+
+"print feature matrix"
+for _, row in df.iterrows():
+    features = get_features(row["word"])
+    print(f'Word: {row["word"]}')
+    print(features)
+    print()
+"==NOTE terminal might not show all words but it still works; use head()/tail() to verify it=="
