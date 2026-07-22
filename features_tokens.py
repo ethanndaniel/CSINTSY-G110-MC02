fbcea98fa9dd27ca4fb1b1b6a61a9c6c0c0fe5e7
ethanndaniel <ethan_daniel_magbatoc@dlsu.edu.com.ph>
2026-07-22 18:06:10 +0800

    added more features and created the feature matrix

--- features_tokens.py
+++ features_tokens.py
@@ -1,14 +1,23 @@
-"Convert words to numerical features to be used in Decision Trees / Naive Bayes algorithms"
+import string
 
 
-
-"add more possible features here"
 def get_features(word):
     return {
+    "==counts=="
         "length": len(word),
+        "alphabet_count": sum(c.isalpha() for c in word),
+        "digit_count": sum(c.isdigit() for c in word),
+
+    "==booleans=="
         "has_digit": any(c.isdigit() for c in word),
-        "has_alpha": any(c.isalpha() for c in word),
+        "has_alphabet": any(c.isalpha() for c in word),
+        "has_hyphen": "-" in word,
+
+        "is_alphabet": word.isalpha(),
         "is_upper": word.isupper(),
         "is_lower": word.islower(),
-        "is_capital": word[0].isupper()
+        "is_capital": word[0].isupper(),
+
+        "is_punctuation": all(c in string.punctuation for c in word),
+
     }
--- tokens.py
+++ tokens.py
@@ -6,10 +6,20 @@ OTH = "OTH"
 
 
 import pandas as pd
+from features_tokens import get_features
 
 "read Sentence IDs 2180 to 2200"
 "FORMAT: word_id, sentence_id, sentence, word, label"
 df = pd.read_excel("labeled_tokens.xlsx")
-print(df.head())
+
+
+"matrix of features and labels for each word"
+x = []
+y = []
+
+"read through each row and get its features"
+for _, row in df.iterrows():
+    x.append(get_features(row["word"]))
+    y.append(row["label"])
 
 
