282c0d4df870f03669a6ced1b3f670c820eb9768
ethanndaniel <ethan_daniel_magbatoc@dlsu.edu.com.ph>
2026-07-26 00:54:56 +0800

    improved several feature logics to handle empty values and single letter words

--- features_tokens.py
+++ features_tokens.py
@@ -6,7 +6,10 @@ CONSONANTS = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ"
 
 # 1. General word features are being accounted
 def get_features(word):
-    safe_word = word or ""  # if word is None or empty -> empty string (for edge cases)
+    safe_word = word if isinstance(word, str) else ""  # if word is None or empty -> empty string (for edge cases)
+
+    alphabet_count = sum(c.isalpha() for c in safe_word)
+    first_alphabet = next((c for c in safe_word if c.isalpha()), "")
 
     lower_word = safe_word.lower()
     has_repeated_char = False
@@ -18,7 +21,8 @@ def get_features(word):
     return {
 
         "length": len(safe_word),
-        "alphabet_count": sum(c.isalpha() for c in safe_word),
+        "alphabet_count": alphabet_count,
+        "has_multiple_alphabets": alphabet_count > 1,
         "digit_count": sum(c.isdigit() for c in safe_word),
         "vowel_count": sum(c in VOWELS for c in safe_word),
         "consonant_count": sum(c in CONSONANTS for c in safe_word),
@@ -30,8 +34,10 @@ def get_features(word):
 
         "is_upper": safe_word.isupper(),
         "is_lower": safe_word.islower(),
-        "is_capital": safe_word[:1].isupper() and safe_word[1:].islower(), #:1 is safer than 0 cause 0 returns error if empty
         "is_punctuation": all(c in string.punctuation for c in safe_word) if safe_word else False, # empty should not get treated as punctuation = return false
+        "is_single_alphabet": alphabet_count == 1,
+        "is_capital": bool(first_alphabet) and first_alphabet.isupper(), #improved logic for single letter words
+
 
         "starts_with_vowel": safe_word[:1] in VOWELS,
         "ends_with_vowel": safe_word[-1:] in VOWELS,
@@ -48,7 +54,7 @@ def get_features(word):
 
 # 2. Word features are being compared to previous word for pattern tracing
 def p_recognition(word2): #word2 = previous word
-    safe_word2 = word2 or ""
+    safe_word2 = word2 if isinstance(word2, str) else ""
 
     return{
         "prev_has_token": len(safe_word2) > 0,
@@ -59,7 +65,7 @@ def p_recognition(word2): #word2 = previous word
         "prev_has_hyphen": "-" in safe_word2,
         "prev_is_upper": safe_word2.isupper(),
         "prev_is_lower": safe_word2.islower(),
-        "prev_is_capital": safe_word2[:1].isupper() and safe_word2[1:].islower(),
+        "prev_is_capital": safe_word2[:1].isupper(),
         "prev_is_punctuation": all(c in string.punctuation for c in safe_word2) if safe_word2 else False,
 
         "prev_ends_with_vowel": safe_word2[-1:] in VOWELS,
